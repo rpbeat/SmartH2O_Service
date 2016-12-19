41e20e2f7c156fea8596e664f37e9f4a302ce2de
RubenPereira <rpbeat@gmail.com>
2016-12-19 23:55:02 +0000

    qqfodaa

--- SmartH2O_Service/ServiceLog.svc.cs
+++ SmartH2O_Service/ServiceLog.svc.cs
@@ -36,25 +36,6 @@ namespace SmartH2O_Service
                 return "tens de treinar mais";
         }
 
-        public void ProcessDirectory(string targetDirectory)
-        {
-            // Process the list of files found in the directory.
-            string[] fileEntries = Directory.GetFiles(targetDirectory);
-            foreach (string fileName in fileEntries)
-                ProcessFile(fileName);
-
-            // Recurse into subdirectories of this directory.
-            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
-            foreach (string subdirectory in subdirectoryEntries)
-                ProcessDirectory(subdirectory);
-        }
-
-        // Insert logic for processing found files here.
-        public void ProcessFile(string path)
-        {
-            this.aux += ("Processed file "+path);
-        }
-
         public string SendAlarm(string docc)
         {
             XmlDocument doc = new XmlDocument();
